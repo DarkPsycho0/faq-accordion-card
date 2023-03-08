01c541171635435aa624e8d977f2e3f01b3d2123
Matheus <13matheusilva@gmail.com>
2023-03-08 17:57:56 -0300

    js pronto

---
+++ src/js/index.js
@@ -0,0 +1,11 @@
+const perguntasERespostas = document.querySelectorAll('.item');
+perguntasERespostas.forEach(function (item) {
+    item.addEventListener('click', function () {
+        const itemAtivo = document.querySelector('.ativo');
+        if (itemAtivo) {
+            itemAtivo.classList.remove('ativo');
+        }
+        item.classList.add('ativo');
+    })
+})
+
